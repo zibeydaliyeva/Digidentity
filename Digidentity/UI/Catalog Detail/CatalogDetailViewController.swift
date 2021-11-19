04a47e41dbf1ca6e1bf6b9ed6dcc5ee6c6223e36
zibeydaliyeva <zibeydaliyeva@gmail.com>
2021-11-19 15:20:34 +0100

    CatalogListViewController integrated

---
+++ Digidentity/UI/Catalog Detail/CatalogDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CatalogDetailViewController.swift
+//  Digidentity
+//
+//  Created by Зибейда Алекперли on 19.11.21.
+//
+
+import UIKit
+
+class CatalogDetailViewController: BaseViewController {
+    
+    var viewModel: CatalogCellViewModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
